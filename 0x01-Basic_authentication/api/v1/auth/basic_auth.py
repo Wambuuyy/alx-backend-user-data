227a30e29f5b9936fae2de989484c46735dbd8aa
Wambuuyy <wambuiprudy@gmail.com>
2024-08-06 22:50:14 +0300

    . Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Basic Authentication module
 """
 
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -37,3 +38,18 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """
+        Extracts the user email and password from the Base64 decoded value
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        user_email, user_password =
+        decoded_base64_authorization_header.split(':', 1)
+        return user_email, user_password
