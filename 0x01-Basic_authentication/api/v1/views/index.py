9107e0ce70243f6b995afd4dacf1cfb67daf6fce
Wambuuyy <wambuiprudy@gmail.com>
2024-08-14 09:27:38 +0300

    custom handler for 403 and a route to trigger the error

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def unauthorized() -> None:
     """raises a 401 error"""
     abort(401)
+
+
+@app_views.route('/forbidden/', methods=['GET'], strict_slashes=False)
+def forbidden() -> None:
+    """raises a 403 error"""
+    abort(403)
