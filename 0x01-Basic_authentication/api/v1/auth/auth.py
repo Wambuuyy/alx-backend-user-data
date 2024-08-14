989fb8ad83e81b33930137db720d50d62328f4db
Wambuuyy <wambuiprudy@gmail.com>
2024-08-14 11:18:57 +0300

    modify auth.py authorization header returns none if authorization not in header or if none else the value of the request creates an auth variable loads AUTH_TYPE checks if it is none instantiates it there are filtering of each requests in the before request handler

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -23,8 +23,10 @@ class Auth:
         return True
 
     def authorization_header(self, request=None) -> str:
-        """returns None -request will be the flask request obj"""
-        return None
+        """validates all requests to secure the API"""
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """returns none - request will be a flask request obj"""
