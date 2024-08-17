3e67c58f81d4181cfb3db0bbb9293f757c6c3aba
Wambuuyy <wambuiprudy@gmail.com>
2024-08-17 10:18:45 +0300

    add a decoding finction returning the decoded value of a base64 string'utf-8 encoding

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """basic authorization"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """
-    empty for now
+    basic authentication class
     """
 
     def extract_base64_authorization_header(self,
@@ -19,3 +20,18 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        """returns a decoded value of a base 64 string"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            # use base64.b64decode to decode base64 string into bytes
+            # .decode('utf-8') converts the bytes into a utf-8 string
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            return None
