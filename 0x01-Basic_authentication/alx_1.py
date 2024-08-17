4b5291fec4e68eefaa6a540b1f76725d18718818
Wambuuyy <wambuiprudy@gmail.com>
2024-08-17 10:50:58 +0300

    the user email if ot linked to any user must return none so used exception to correct that

--- 0x01-Basic_authentication/alx_1.py
+++ 0x01-Basic_authentication/alx_1.py
@@ -1,32 +1,14 @@
 #!/usr/bin/python3
 """ Check response
 """
-import requests
 
 if __name__ == "__main__":
-    r = requests.get('http://0.0.0.0:3456/api/v1/users', headers={ 'Authorization': "HBTN" })
-    if r.status_code != 403:
-        print("Wrong status code: {}".format(r.status_code))
-        exit(1)
-    if r.headers.get('content-type') != "application/json":
-        print("Wrong content type: {}".format(r.headers.get('content-type')))
+    from api.v1.auth.basic_auth import BasicAuth
+
+    ba = BasicAuth()
+    res = ba.user_object_from_credentials("u1@gmail.com", "pwd")
+    if res is not None:
+        print("user_object_from_credentials must return None if 'user_email' is not linked to any user")
         exit(1)
     
-    try:
-        r_json = r.json()
-        
-        if len(r_json.keys()) != 1:
-            print("Not the right number of element in the JSON: {}".format(r_json))
-            exit(1)
-        
-        error_value = r_json.get('error')
-        if error_value is None:
-            print("Missing 'error' key in the JSON: {}".format(r_json))
-            exit(1)
-        if error_value != "Forbidden":
-            print("'error' doesn't have the right value: {}".format(error_value))
-            exit(1)
-            
-        print("OK", end="")
-    except:
-        print("Error, not a JSON")
+    print("OK", end="")
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -56,7 +56,10 @@ class BasicAuth(Auth):
             return None
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
-        users = User.search({'email': user_email})
+        try:
+            users = User.search({"email": user_email})
+        except Exception:
+            return None
         if not users or len(users) == 0:
             return None
         user = users[0]
